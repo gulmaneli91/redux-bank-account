053230e4239eadc54bcea01b23cd007f1ac8c189
Ailton Gulmaneli <ailton_gulmaneli@hotmail.com>
2025-02-08 00:02:24 -0300

    apply thunks for convert currence

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -13,6 +13,7 @@ const accountSlice = createSlice({
   reducers: {
     deposit(state, action) {
       state.balance += action.payload;
+      state.isLoading = false;
     },
     withdraw(state, action) {
       state.balance -= action.payload;
@@ -35,11 +36,25 @@ const accountSlice = createSlice({
       state.loanPurpose = "";
       state.loan = 0;
     },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
   },
 });
-export const { deposit, payLoan, requestLoan, withdraw } = accountSlice.actions;
+export const { payLoan, requestLoan, withdraw } = accountSlice.actions;
 
-console.log(payLoan);
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    const res = await fetch(
+      `https://api.frankfurter.dev/v1/latest?base=${currency}&symbols=USD`
+    );
+    const data = await res.json();
+    const converted = +(amount * data.rates.USD).toFixed(2);
+    dispatch({ type: "account/deposit", payload: converted });
+  };
+}
 export default accountSlice.reducer;
 
 // export default function accountReducer(state = initialState, action) {
